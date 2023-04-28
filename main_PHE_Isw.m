f5895d069a84c5ee156735653cd9f5e73bd639ea
probe station <bcmcgold@mit.edu>
2023-04-28 12:47:03 -0400

    Adding PHE measurement code

--- main_PHE_Isw.m
+++ main_PHE_Isw.m
@@ -5,7 +5,19 @@ instrreset;
 motor.obj = serialport('COM3',9600); pause(2);
 motor.name = 'motor';
 
+sourcemeter.obj = gpib('ni',0,4); fopen(sourcemeter.obj);
+sourcemeter.name = 2400;
+
+voltmeter.obj = gpib('ni',0,5); fopen(voltmeter.obj);
+voltmeter.name = 2400;
+
+daq.obj = daq("mcc"); daq.field_factor = 328;
+daq.name = 'daq';
+
 %%
+output.field = 0;
+
+% motor starts along y-axis. Rotate to x-axis to apply reset field
 set_inst(motor,'Angle',90);
 
 save output;
