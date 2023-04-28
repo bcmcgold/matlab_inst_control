2ee7a18f70639ce0f4a2fbc08fec69d3db671a3b
probe station <bcmcgold@mit.edu>
2023-04-28 13:04:18 -0400

    Adding motor rotation code

--- set_motor.m
+++ set_motor.m
@@ -1,7 +1,14 @@
+% This function can be used to communicate with stepper motor via serial
+% port interfaced with Arduino. Run MotorStepSerial
 function set_motor( inst_obj , set_mode , set_value )
 
     switch set_mode
         case {'Angle'}
+            writeline(inst_obj,num2str(set_value)); % write angle setting to serial port
+            read(inst_obj,1,"uint8"); % pause until motor finished rotating
+            flush(inst_obj)
+        case {'Angle2'}
+            load('_motor_current_angle');
             writeline(inst_obj,num2str(set_value)); % write angle setting to serial port
             read(inst_obj,1,"uint8"); % pause until motor finished rotating
             flush(inst_obj)
