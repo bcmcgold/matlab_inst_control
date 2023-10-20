7d0846f7f93a7215356ba23dd7173c1e249eeb3c
BCM <bcmcgold@mit.edu>
2023-10-20 13:37:37 -0400

    scope dual current code

---
+++ main_scope_dual_current.m
@@ -0,0 +1,150 @@
+%% notes & prerequisites
+% scope must be set up on channel 3
+
+%% initialize
+clear all;
+close all;
+instrreset;
+
+%% measurement parameters
+output.chip = cellstr("S2302153_300C_H1");
+output.device = cellstr("1-18");
+output.other_notes = cellstr("");
+output.sense_R = 19.7; % kOhms
+output.channel_R = .334; % kOhms, channel contribution to MTJ resistance (IMPORTANT for synchronizing two sources)
+output.gain = 2/2; % divide by 2 to account for attenuation of 50-ohm connection
+output.H = -31; % Oe
+output.wait_after_I = 0.5; % s
+output.n_readings = 1;
+output.wait_between_readings = 0.1; % s
+
+% Vmtj, Isot sweeps
+output.mtj_current = [-26e-3]; % mA
+% output.mtj_current = linspace(-.8,.8,20); % mA
+% output.mtj_current = [output.mtj_current flip(output.mtj_current)];
+% output.sot_current = [0]; % mA
+output.sot_current = linspace(-1,1,20); % mA
+output.sot_current = [output.sot_current flip(output.sot_current)];
+
+%% connect and set up instruments & files
+% automatically set up data folders
+date_id = datestr(now,'yyyymmDD');
+data_folder = "D:/"+date_id+"/scope_output_"+time_id+"/";
+mkdir(data_folder)
+
+% use one 2400 as source and voltmeter on MTJ
+% source voltage/measure current mode
+mtj_src.obj = gpib('ni',0,5); fopen(mtj_src.obj);
+mtj_src.name = 2400;
+
+% use another 2400 as source and voltmeter on current channel
+% source current/measure voltage
+sot_src.obj = gpib('ni',0,24); fopen(sot_src.obj);
+sot_src.name = 2400;
+
+% MCC DAQ for field
+field.obj = daq("mcc");
+field.name = 'daq';
+field.field_factor = 620;
+
+% connect to motor in case we want to rotate magnet after measurement
+motor.obj = serialport('COM3',9600); pause(2);
+motor.name = 'motor';
+% set_inst(motor,'Angle_simple',xx) reminder of motor turn command
+
+% set up scope
+scope_tcp = tcpip("169.254.47.225",80);
+scope = icdevice('lecroy_basic_driver.mdd', scope_tcp);
+connect(scope); % if connect fails: turn TCP on/off on scope
+% setting any of MEAS1-4 just changes them all
+set(scope.MEAS1,'Source','channel3')
+% set trigger point to left edge of screen
+set(scope.acquisition,'Delay',-scope.acquisition.timebase*5);
+
+%% iterate over all combinations of variables, measure, and plot
+% ramp up field
+ramp_inst(field,'field IP',output.H(1),5);
+
+i=1; % index for saving output
+for mc = output.mtj_current
+    % make one figure per mtj current
+    figure;
+    h = animatedline('Marker','o');
+    xlabel("I_{SOT} (mA)")
+    ylabel("R_{MTJ} (kohm)")
+    title("I_{MTJ}="+mc+"mA")
+
+    % first, take a reading of MTJ current alone
+    ramp_inst(sot_src,'mA',0,5);
+    set_inst(mtj_src,'mA',mc);
+    pause(output.wait_after_I);
+    Vmtj=read_inst(mtj_src,'XV')-output.sense_R*mc;
+    set_inst(mtj_src,'mA',0);
+    ramp_inst(sot_src,'mA',output.sot_current(1),5);
+    for sc = output.sot_current
+        % set SOT current
+        set_inst(sot_src,'mA',sc);
+        pause(output.wait_after_I);
+
+        % adjust scope parameters
+        Voffset = read_inst(mtj_src,'XV'); % V from channel
+        scope_trig = Voffset + Vmtj;
+        if Vmtj > 0
+            scope_trig = scope_trig-get(scope.C3,'Scale')*2;
+        else
+            scope_trig = scope_trig+get(scope.C3,'Scale')*2;
+        end
+        set(scope.Trigger1,'Level',scope_trig);
+        set(scope.C3,'Position',-Voffset-Vmtj)
+
+        % trigger scope and pulse current
+        invoke(scope.trigger,'trigger');
+        pause(0.5);
+        tic;
+        set_inst(mtj_src,'mA',mc);
+        while scope.acquisition.state ~= "stop" % wait for scope to trigger
+        end
+        [scopedata.y, scopedata.t] = invoke(scope.waveform, 'readwaveform', 'channel3');
+        save(data_folder+strrep(sprintf("timetraceH%gOen%d",output.H(i),i),'.','p')+".mat","scopedata");
+        clear scopedata
+        toc
+
+        set(scope.MEAS1,'MeasurementType','mean');
+        output.Vmean(i) = get(scope.MEAS1).Value/output.gain;
+        output.Vmtj_scope(i)=output.Vmean(i)-Voffset-mc*output.channel_R; % V
+        
+        % measure from Keithley (helps for finding errors)
+        output.t(i) = str2double(datestr(now,'HHMMSS'));
+        output.Vmtj_raw(i) = read_inst_avg(mtj_src,'XV',output.n_readings,output.wait_between_readings); % V
+        output.Vmtj_subtr(i) = output.Vmtj_raw(i)-mc*output.sense_R-Voffset-mc*output.channel_R; % V
+        output.Vchan(i) = read_inst(sot_src,'XV'); % V
+        set_inst(mtj_src,'mA',0)
+
+        % % set scope offset & range appropriately
+        % scope_position = output.Vmtj_raw(i)-mc*output.sense_R;
+        % set(scope.C3,'Position',-scope_position);
+        % set(scope.Trigger1,'Level',scope_position);
+        % % set(scope.trigger,'Mode','single');
+        % invoke(scope.trigger,'trigger'); % trigger scope
+        % while scope.acquisition.state ~= "stop" % wait for scope to trigger
+        % end
+
+        output.R(i)=output.Vmtj_scope(i)/mc;
+        
+        % update plot
+        addpoints(h,sc,output.R(i));
+        drawnow
+
+        % increment counter
+        i=i+1;
+    end
+end
+
+% ramp down sources
+ramp_inst(field,'field IP',0,5);
+ramp_inst(mtj_src,'mA',0,5);
+ramp_inst(sot_src,'mA',0,5);
+    
+save(data_folder+output.chip+"_"+output.device+"_DualI_"+output.other_notes+"_"+datestr(now,'HHMM')+".mat","output");
+% save figures
+saveas(h,data_folder+output.chip+"_"+output.device+"_DualI_"+output.other_notes+"_"+datestr(now,'HHMM')+".jpg");
